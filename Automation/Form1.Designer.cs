d15ac5cd573148de1ddc59c4cb969e93a7e5d71f
ExpertBC <top2successfulman@gmail.com>
2022-07-31 21:23:44 +1000

    Initial commit

---
+++ Automation/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Automation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(369, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(111, 38);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(12, 72);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSource.Size = new System.Drawing.Size(686, 391);
+            this.txtSource.TabIndex = 1;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(155, 28);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(116, 38);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 475);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Automation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
+
---
+++ Automation/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+using OpenQA.Selenium.Chrome; //module for control web browser.
+using OpenQA.Selenium.Support.UI; //module for control web browser.
+using OpenQA.Selenium; //module for control web browser.
+using WebDriverManager.DriverConfigs.Impl; //module for web browser.
+
+using HtmlAgilityPack;
+
+namespace Automation
+{
+    public partial class Form1 : Form
+    {
+        ChromeDriver driver;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // start automation.
+            Task task = Task.Run((System.Action)AutomationTask);
+        }
+
+        public void AutomationTask()
+        {
+            // switch to current activated tab from browser.
+            String parentWindowHandle = driver.CurrentWindowHandle;
+            driver.SwitchTo().Window(parentWindowHandle);
+
+            // go to "google.com".
+            driver.Url = "https://www.google.com/";
+            // input "C# tutorial" for search.
+            driver.FindElement(By.XPath("//input[@name='q']")).SendKeys("C# tutorial");
+            // I did ready 1 second for you need to see the screen.  You can remove this code.
+            Thread.Sleep(1000);
+            // start search.
+            driver.FindElement(By.XPath("//input[@name='btnK']")).Click();
+            // I did ready 1 second for you need to see the screen.  You can remove this code.
+            Thread.Sleep(1000);
+            // click the C# tutorial link.
+            driver.FindElement(By.XPath("//div[@id='search']/div/div/div[2]/div/div/div[1]/a")).Click();
+            // get page source from web browser.
+            string htmlCode = driver.PageSource;
+            if (txtSource.InvokeRequired)
+            {
+                txtSource.Invoke(new System.Action(() => txtSource.Text = htmlCode));
+            }
+            else
+            {
+                txtSource.Text = htmlCode;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // open the chrome browser.
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeOptions options = new ChromeOptions();
+            //options.AddArguments(@"--user-data-dir=C:\Users\Develop\AppData\Local\Google\Chrome\User Data\Profile 1");
+            options.AddExtensions("MetaMask.crx");
+            //options.AddAdditionalCapability("browserstack.safari.allowAllCookies", true);
+            driver = new ChromeDriver(options);
+            
+            String parentWindowHandle = driver.CurrentWindowHandle;
+            driver.SwitchTo().Window(parentWindowHandle);
+            driver.Url = "https://www.google.com/";
+            //driver.Manage().Cookies.DeleteAllCookies();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            // switch to current activated tab from browser.
+            String parentWindowHandle = driver.CurrentWindowHandle;
+            driver.SwitchTo().Window(parentWindowHandle);
+
+            // go to "google.com".
+            driver.Url = "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#initialize/create-password/import-with-seed-phrase";
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("escape call robust wolf bleak treat measure multiply gesture unfold tilt gravity");
+            driver.FindElement(By.XPath("//input[@id='password']")).SendKeys("test1234");
+            driver.FindElement(By.XPath("//input[@id='confirm-password']")).SendKeys("test1234");
+            driver.FindElement(By.XPath("//div[@class='first-time-flow__checkbox first-time-flow__terms']")).Click();
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//button[@role='button']")).Click();
+        }
+    }
+}
